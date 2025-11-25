1ee8015b340de61c19463a86fa521b10924769aa
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2025-11-25 15:49:28 +0000

    added templates

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from .models import Post
 
 class PostList(generic.ListView):
     #model = Post
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    #queryset = Post.objects.all()
     template_name = "post_list.html"
 
 
