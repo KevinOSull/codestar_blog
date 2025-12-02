97afa03ad654a0056f9849064304769c374fd382
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2025-12-02 23:18:11 +0000

    added cloudinary api for images

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-3!s@)j6w*j1a9izw)w##(=-xrk4+b9w6gkjw5e@yvw4hm$a82x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
